e3d473aa4bc5c25686140f53c4780a030368254f
Bruno Vilefort <bruno.clara@yahoo.com>
2022-05-10 15:09:31 -0300

    Página principal da aplicação

---
+++ app.js
@@ -0,0 +1,55 @@
+// External modules:
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+const cowsay = require("cowsay");
+
+// Core modules:
+const fs = require("fs");
+
+// Welcome message:
+console.log(
+    cowsay.say({text: chalk.bgGreen.black`Bem-vindo ao Muuhbank`, e: "^^"})
+);
+
+// Starting the options menu:
+operation();
+
+// Primary options:
+function operation() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "action",
+            message: "O que deseja fazer?\n",
+            choices: [
+                "Criar Conta",
+                "Consultar Saldo",
+                "Depositar",
+                "Sacar",
+                "Sair"
+            ]
+        },
+    ])
+    .then((answer) => {
+        const action = answer["action"]
+        if (action === "Criar Conta") {
+            const createAccount = require("./functionalities/create_account");
+            createAccount()
+        } else if (action === "Consultar Saldo") {
+
+        } else if (action === "Depositar") {
+
+        } else if (action === "Sacar") {
+
+        } else if (action === "Sair") {
+            console.log(
+                cowsay.say({text: chalk.bgBlueBright`Obrigado por utilizar o Muuhbank!`, e: "^^"})
+            );
+            process.exit();
+        }
+        
+    })
+    .catch((err) => console.log(err))
+};
+
+module.exports = operation;
